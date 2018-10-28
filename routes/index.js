e9d219850f24c2180e6d3c1a6bd4f2fe9467bcc3
AlexDevOp <29019119+alexanderkoz@users.noreply.github.com>
2018-10-27 22:43:03 -0400

    Routes

--- routes/index.js
+++ routes/index.js
@@ -3,95 +3,108 @@ var router = express.Router();
 var expressValidator = require('express-validator');
 var passport = require('passport');
 /* GET home page. */
-router.get('/', function(req, res){
-  console.log(req.user);
-  console.log(req.isAuthenticated())
-  res.render('home',{title: 'home'});
+router.get('/', function(req, res) {
+	console.log(req.user);
+	console.log(req.isAuthenticated())
+	res.render('home', {
+		title: 'home'
+	});
+});
+router.get('/add_class', function(req, res) {
+	res.render('add_class')
+});
+router.get('/profile', authenticationMiddleware(), function(req, res) {
+	res.render('profile', {
+		title: 'profile'
+	});
 });
 
-router.get('/profile', authenticationMiddleware(),function(req, res){
-  res.render('profile',{title:'profile'});
-});
-
-router.get('/login',function(req, res){
-  res.render('login', {title: 'Login'});
+router.get('/login', function(req, res) {
+	res.render('login', {
+		title: 'Login'
+	});
 });
 
 router.post('/login', passport.authenticate(
-    'local',{
-      successRedirect: '/profile',
-      failureRedirect: '/login'
+	'local', {
+		successRedirect: '/student_panel',
+		failureRedirect: '/login'
 
-}));
+	}));
 
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Registration' });
+	res.render('register', {
+		title: 'Registration'
+	});
 });
 
 
 router.post('/register', function(req, res, next) {
-  //checn input if its valid
-  req.checkBody('username', 'Username field cannot be empty.').notEmpty();
-  req.checkBody('username', 'Username field cannot be empty.').notEmpty();
-  req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
-  req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
-  req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
-  req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
-  req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
-  req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8, 100);
-  req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(req.body.password);
+	//checn input if its valid
+	req.checkBody('username', 'Username field cannot be empty.').notEmpty();
+	req.checkBody('username', 'Username field cannot be empty.').notEmpty();
+	req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
+	req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
+	req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
+	req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
+	req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+	req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8, 100);
+	req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(req.body.password);
 
-  const errors = req.validationErrors();
-    if(errors){
+	const errors = req.validationErrors();
+	if (errors) {
 
-      console.log(`errors: ${JSON.stringify(errors)}`);
+		console.log(`errors: ${JSON.stringify(errors)}`);
 
-      res.render('register', {
-        title: 'Registration Error',
-        errors: errors
-      } );
-    } else {
-      const fname = req.body.fname;
-      const lname = req.body.lname;
-      const username = req.body.username;
-      const email = req.body.email;
-      const password = req.body.password;
+		res.render('register', {
+			title: 'Registration Error',
+			errors: errors
+		});
+	} else {
+		const first_name = req.body.first_name;
+		const last_name = req.body.last_name;
+		const username = req.body.username;
+		const email = req.body.email;
+		const password = req.body.password;
 
-      const db = require('../db.js');
-      //MAKE QUERY TO POST DATA TO database
-      db.query('INSERT INTO user (fname, lname, username, email, password) VALUES (?,?,?,?,?)', [fname, lname, username, email, password], function(error, results, fields){
-        if(error) throw error;
+		const db = require('../db.js');
+		//MAKE QUERY TO POST DATA TO database
+		db.query('INSERT INTO user (first_name, last_name, username, email, password) VALUES (?,?,?,?,?)', [first_name, last_name, username, email, password], function(error, results, fields) {
+			if (error) throw error;
 
-          db.query('SELECT LAST_INSERT_ID() as user_id', function(error, results, fields){
-              if(error) throw error;
+			db.query('SELECT LAST_INSERT_ID() as user_id', function(error, results, fields) {
+				if (error) throw error;
 
-              const user_id = results[0];
+				const user_id = results[0];
 
-              console.log(results[0]);
-              req.login(user_id ,function(err){
-                  res.redirect('/login');
+				console.log(results[0]);
+				req.login(user_id, function(err) {
+					res.redirect('/login');
 
-              });
-          })
-      })
-    }
+				});
+			})
+		})
+	}
 
 
 });
 
 passport.serializeUser(function(user_id, done) {
-  done(null, user_id);
+	done(null, user_id);
 });
 
 passport.deserializeUser(function(user_id, done) {
-    done(null, user_id);
+	done(null, user_id);
 });
-  function authenticationMiddleware(){
-    return (req, res, next)=>{
-      console.log( `req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
 
-      if (req.isAuthenticated()) return next();
-      res.redirect('/profile')
-    }
-  }
+function authenticationMiddleware() {
+	return (req, res, next) => {
+		console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
+
+		if (req.isAuthenticated()) return next();
+		res.redirect('/profile')
+	}
+}
+
+
 module.exports = router;
